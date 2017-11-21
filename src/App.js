aef0a4f13c8f36a0796067b577a0fcf707fe0bee
Mateo Clarke <code@mateoclarke.com>
2017-11-21 10:08:10 -0600

    Revert "Add basic routing for service pages"

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
-import ServicesIndex from "./ServicesIndex"
-import Service from "./Service"
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <header className="App-header">
-            <p className="App-intro">
-              Hello Janis <span role="img" aria-label="peace sign">☮️</span>
-            </p>
-          </header>
-          <Route path="/services" component={ServicesIndex} />
-          <Route path="/service/:id" component={Service} />
-        </div>
-      </Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Hello Janis <span role="img" aria-label="peace sign">☮️</span>
+        </p>
+      </div>
     );
   }
 }
